15df34dda97998205856a3b6a5c05cc10406a40f
Juan David Villada <juan.villadar@upb.edu.co>
2025-07-17 11:04:11 -0500

    Creamos la carpeta src y agragamos el archivo hola-mundo.py

---
+++ src/hola-mundo.py
@@ -0,0 +1,11 @@
+# mi_primer_script.py
+print("¡Hola, GitHub!")
+for i in range(5)
+    print(f"Este es el mensaje numero (i+1)")
+print (" Adios, github")
+
+a=10 
+while a <10 
+print("hola")
+a=a+1
+
